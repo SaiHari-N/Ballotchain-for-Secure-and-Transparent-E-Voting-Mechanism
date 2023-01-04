c939ccd0dafe7e068a33310f2c9b9a72833aedb5
SaiHari_N <74491747+SaiHari-N@users.noreply.github.com>
2023-01-04 15:22:02 +0530

    Add files via upload

---
+++ include/link_home.php
@@ -0,0 +1,75 @@
+<ul class="nav" id="main-menu">
+
+                    <li>
+                        <a href="index_log.php"><i class="fa fa-dashboard"></i> Home</a>
+                    </li>
+					<li>
+                        <a href="login_vote.php"><i class="fa fa-dashboard"></i> Polling</a>
+                    </li>
+                    <li>
+                        <a href="login.php"><i class="fa fa-desktop"></i> Election Commision</a>
+                    </li>
+					<li>
+                        <a href="login_auth.php"><i class="fa fa-desktop"></i> Election Authority</a>
+                    </li>
+					<li>
+                        <a href="result.php"><i class="fa fa-desktop"></i> Result</a>
+                    </li>
+					<li>
+                        <a href="logout2.php"><i class="fa fa-desktop"></i> Exit</a>
+                    </li>
+					 
+					<!-- <li>
+                        <a href="#"><i class="fa fa-sitemap"></i> Charts<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="chart.html">Charts JS</a>
+                            </li>
+                            <li>
+                                <a href="morris-chart.html">Morris Chart</a>
+                            </li>
+							</ul>
+						</li>	
+                    <li>
+                        <a href="tab-panel.html"><i class="fa fa-qrcode"></i> Tabs & Panels</a>
+                    </li>
+                    
+                    <li>
+                        <a href="table.html"><i class="fa fa-table"></i> Responsive Tables</a>
+                    </li>
+                    <li>
+                        <a href="form.html" class="active-menu"><i class="fa fa-edit"></i> Forms </a>
+                    </li>
+
+
+                    <li>
+                        <a href="#"><i class="fa fa-sitemap"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="#">Second Level Link</a>
+                            </li>
+                            <li>
+                                <a href="#">Second Level Link</a>
+                            </li>
+                            <li>
+                                <a href="#">Second Level Link<span class="fa arrow"></span></a>
+                                <ul class="nav nav-third-level">
+                                    <li>
+                                        <a href="#">Third Level Link</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Third Level Link</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Third Level Link</a>
+                                    </li>
+
+                                </ul>
+
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="empty.html"><i class="fa fa-fw fa-file"></i> Empty Page</a>
+                    </li>-->
+                </ul>
